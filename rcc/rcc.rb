3bd4481212ab256ea533e723f13e5f84dc9b8f1a
yasaichi <yasaichi@users.noreply.github.com>
2019-05-03 19:12:54 +0900

    [rcc] 足し算と引き算を追加

--- rcc/rcc.rb
+++ rcc/rcc.rb
@@ -1,20 +1,47 @@
+# frozen_string_literal: true
+require 'stringio'
+require 'strscan'
+
 module Rcc
-  def self.call(code)
-    <<~ASM
-      .intel_syntax noprefix
-      .global main
-      main:
-        mov rax, #{code.to_i}
-        ret
-    ASM
+  INTEGER_PATTERN = /[1-9]\d*|0/
+
+  class << self
+    def call(code)
+      ".intel_syntax noprefix\n#{parse(code)}\n"
+    end
+
+    private
+
+    def parse(code)
+      asm = StringIO.new
+      asm.puts <<~CODE
+        .global main
+        main:
+      CODE
+
+      scanner = StringScanner.new(code)
+      initial = scanner.scan(INTEGER_PATTERN)
+      raise "予期しない文字です: #{scanner.getch}" unless initial
+
+      asm.puts("  mov rax, #{initial}")
+      until scanner.eos? do
+        case operator = scanner.getch
+        when '+'
+          asm.puts("  add rax, #{scanner.scan(INTEGER_PATTERN)}")
+        when '-'
+          asm.puts("  sub rax, #{scanner.scan(INTEGER_PATTERN)}")
+        else
+          raise "予期しない文字です: #{operator}"
+        end
+      end
+      asm.puts("  ret")
+
+      asm.string
+    end
   end
 end
 
 if __FILE__ == $0
-  unless ARGV.length == 1
-    STDERR.puts '引数の個数が正しくありません'
-    exit 1
-  end
-
+  raise '引数の個数が正しくありません' unless ARGV.length == 1
   puts Rcc.call(ARGV[0])
 end
--- rcc/rcc_test.rb
+++ rcc/rcc_test.rb
@@ -4,16 +4,20 @@ require 'minitest/autorun'
 require 'minitest/spec'
 require_relative 'rcc'
 
+# NOTE: expected, code
+parameters = [
+  [42, '42'],
+  [0, '0'],
+  [21, '5+20-4']
+]
+
 describe Rcc do
   before do
     @tmp = 'tmp'
   end
 
-  it 'compiles an input integer' do
-    [
-      [42, '42'],
-      [0, '0']
-    ].each do |expected, code|
+  it 'compiles code written in the subset of C correctly' do
+    parameters.each do |expected, code|
       _, status = Open3.capture2(
         "gcc -x assembler - -o #{@tmp} && ./#{@tmp}",
         stdin_data: Rcc.call(code)
